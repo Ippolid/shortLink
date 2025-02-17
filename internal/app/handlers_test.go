239de4be7d62fad2fec837c5c1cd8abe55bb381c
Ippolid <iliamishin2006@gmail.com>
2025-02-17 21:54:25 +0300

    edit test, add resty

--- internal/app/handlers_test.go
+++ internal/app/handlers_test.go
@@ -3,15 +3,73 @@ package app
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"io"
+	"gopkg.in/resty.v1"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 )
 
-func TestСreateLink(t *testing.T) {
+//	func TestСreateLink(t *testing.T) {
+//		db := NewDbase()
+//		type want struct {
+//			code        int
+//			response    string
+//			contentType string
+//		}
+//		tests := []struct {
+//			name    string
+//			request string
+//			want    want
+//		}{
+//			{
+//				name:    "positive test #1",
+//				request: "https://ru.wikipedia.org/wiki/SHA-1",
+//				want: want{
+//					code:        201,
+//					response:    "http://localhost:8080/b12a6809",
+//					contentType: "text/plain",
+//				},
+//			},
+//			{
+//				name:    "positive test #2",
+//				request: "https://github.com/Ippolid/shortLink/pulls?q=is%3Apr+is%3Aopen",
+//				want: want{
+//					code:        201,
+//					response:    `http://localhost:8080/14603b1d`,
+//					contentType: "text/plain",
+//				},
+//			},
+//		}
+//		for _, test := range tests {
+//			t.Run(test.name, func(t *testing.T) {
+//				server := New(&db) // создаём новый экземпляр сервера
+//				handler := ValidationMiddleware(http.HandlerFunc(server.PostCreate))
+//				request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(test.request))
+//				request.Header.Set("Content-Type", "text/plain")
+//				// создаём новый Recorder
+//				w := httptest.NewRecorder()
+//				handler.ServeHTTP(w, request)
+//
+//				res := w.Result()
+//				// проверяем код ответа
+//				assert.Equal(t, res.StatusCode, test.want.code)
+//				// получаем и проверяем тело запроса
+//				defer res.Body.Close()
+//				resBody, err := io.ReadAll(res.Body)
+//
+//				require.NoError(t, err)
+//				assert.Equal(t, string(resBody), test.want.response)
+//				assert.Equal(t, res.Header.Get("Content-Type"), test.want.contentType)
+//			})
+//		}
+//	}
+func TestCreateLink(t *testing.T) {
 	db := NewDbase()
+	server := New(&db)
+	r := server.newServer()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
 	type want struct {
 		code        int
 		response    string
@@ -41,34 +99,91 @@ func TestСreateLink(t *testing.T) {
 			},
 		},
 	}
+
+	client := resty.New()
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			server := New(&db) // создаём новый экземпляр сервера
-			handler := ValidationMiddleware(http.HandlerFunc(server.PostCreate))
-			request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(test.request))
-			request.Header.Set("Content-Type", "text/plain")
-			// создаём новый Recorder
-			w := httptest.NewRecorder()
-			handler.ServeHTTP(w, request)
-
-			res := w.Result()
-			// проверяем код ответа
-			assert.Equal(t, res.StatusCode, test.want.code)
-			// получаем и проверяем тело запроса
-			defer res.Body.Close()
-			resBody, err := io.ReadAll(res.Body)
+			resp, err := client.R().
+				SetBody(test.request).
+				Post(ts.URL + "/")
 
 			require.NoError(t, err)
-			assert.Equal(t, string(resBody), test.want.response)
-			assert.Equal(t, res.Header.Get("Content-Type"), test.want.contentType)
+			assert.Equal(t, test.want.code, resp.StatusCode())
+			assert.Equal(t, test.want.response, resp.String())
+			assert.Equal(t, test.want.contentType, resp.Header().Get("Content-Type"))
 		})
 	}
 }
+
+//	func TestGetLink(t *testing.T) {
+//		db := NewDbase()
+//		server := New(&db)
+//		server.database.SaveLink([]byte("https://ru.wikipedia.org/wiki/SHA-1"), "b12a6809")
+//		server.database.SaveLink([]byte("https://github.com/Ippolid/shortLink/pulls?q=is%3Apr+is%3Aopen"), "14603b1d")
+//		type want struct {
+//			code     int
+//			location string
+//		}
+//		tests := []struct {
+//			name    string
+//			request string
+//			want    want
+//		}{
+//			{
+//				name:    "positive test #1",
+//				request: "http://localhost:8080/b12a6809",
+//				want: want{
+//					code:     307,
+//					location: "https://ru.wikipedia.org/wiki/SHA-1",
+//				},
+//			},
+//			{
+//				name:    "positive test #2",
+//				request: "http://localhost:8080/14603b1d",
+//				want: want{
+//					code:     307,
+//					location: "https://github.com/Ippolid/shortLink/pulls?q=is%3Apr+is%3Aopen",
+//				},
+//			},
+//			{
+//				name:    "negative test #3",
+//				request: "http://localhost:8080/14603b1d123",
+//				want: want{
+//					code:     400,
+//					location: "",
+//				},
+//			},
+//		}
+//		for _, test := range tests {
+//			t.Run(test.name, func(t *testing.T) {
+//				// создаём новый экземпляр сервера
+//				handler := ValidationMiddleware(http.HandlerFunc(server.GetID))
+//				request := httptest.NewRequest(http.MethodGet, test.request, nil)
+//				// создаём новый Recorder
+//				w := httptest.NewRecorder()
+//				handler.ServeHTTP(w, request)
+//
+//				res := w.Result()
+//				defer res.Body.Close()
+//				// проверяем код ответа
+//				assert.Equal(t, res.StatusCode, test.want.code)
+//				// проверяем заголовок Location
+//				location := res.Header.Get("Location")
+//				assert.Equal(t, location, test.want.location)
+//			})
+//		}
+//	}
 func TestGetLink(t *testing.T) {
 	db := NewDbase()
 	server := New(&db)
 	server.database.SaveLink([]byte("https://ru.wikipedia.org/wiki/SHA-1"), "b12a6809")
 	server.database.SaveLink([]byte("https://github.com/Ippolid/shortLink/pulls?q=is%3Apr+is%3Aopen"), "14603b1d")
+
+	r := server.newServer()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
 	type want struct {
 		code     int
 		location string
@@ -80,7 +195,7 @@ func TestGetLink(t *testing.T) {
 	}{
 		{
 			name:    "positive test #1",
-			request: "http://localhost:8080/b12a6809",
+			request: "/b12a6809",
 			want: want{
 				code:     307,
 				location: "https://ru.wikipedia.org/wiki/SHA-1",
@@ -88,7 +203,7 @@ func TestGetLink(t *testing.T) {
 		},
 		{
 			name:    "positive test #2",
-			request: "http://localhost:8080/14603b1d",
+			request: "/14603b1d",
 			want: want{
 				code:     307,
 				location: "https://github.com/Ippolid/shortLink/pulls?q=is%3Apr+is%3Aopen",
@@ -96,29 +211,27 @@ func TestGetLink(t *testing.T) {
 		},
 		{
 			name:    "negative test #3",
-			request: "http://localhost:8080/14603b1d123",
+			request: "/14603b1d123",
 			want: want{
 				code:     400,
 				location: "",
 			},
 		},
 	}
+
+	client := resty.New().
+		SetRedirectPolicy(resty.RedirectPolicyFunc(func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}))
+
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			// создаём новый экземпляр сервера
-			handler := ValidationMiddleware(http.HandlerFunc(server.GetID))
-			request := httptest.NewRequest(http.MethodGet, test.request, nil)
-			// создаём новый Recorder
-			w := httptest.NewRecorder()
-			handler.ServeHTTP(w, request)
+			resp, err := client.R().
+				Get(ts.URL + test.request)
 
-			res := w.Result()
-			defer res.Body.Close()
-			// проверяем код ответа
-			assert.Equal(t, res.StatusCode, test.want.code)
-			// проверяем заголовок Location
-			location := res.Header.Get("Location")
-			assert.Equal(t, location, test.want.location)
+			require.NoError(t, err)
+			assert.Equal(t, test.want.code, resp.StatusCode())
+			assert.Equal(t, test.want.location, resp.Header().Get("Location"))
 		})
 	}
 }
