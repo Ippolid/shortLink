bb2bed216a27a9f67bac4b2884f39998cb2907d2
Ippolid <iliamishin2006@gmail.com>
2025-02-18 20:16:58 +0300

    add config

---
+++ config/config.go
@@ -0,0 +1,10 @@
+package config
+
+import "flag"
+
+func ParseFlags() (string, string) {
+	host := flag.String("a", "localhost:8080", "host")
+	adr := flag.String("b", "https::/localhost:8080/", "adress")
+	flag.Parse()
+	return *host, *adr
+}
