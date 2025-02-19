585ce9a84444560f621dab7db74a85d23e179cb4
Ippolid <iliamishin2006@gmail.com>
2025-02-19 21:58:37 +0300

    bug fixes

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -17,8 +17,8 @@ func main() {
 
 	if envRunAdr := os.Getenv("BASE_URL"); envRunAdr != "" {
 		adr = envRunAdr
+		adr += "/"
 	}
-	adr += "/"
 	k := app.NewDbase()
 	s := app.New(&k, adr, host)
 	if err := s.Start(); err != nil {
