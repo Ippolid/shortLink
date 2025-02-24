9730958a969bf26e0cdf61e139a0508577a6550f
Ippolid <iliamishin2006@gmail.com>
2025-02-24 23:42:25 +0300

    edit conf

--- config/config.go
+++ config/config.go
@@ -24,7 +24,8 @@ func ParseFlags() (string, string, string) {
 		*host = envHost
 	}
 	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
-		*baseURL = envBaseURL + "/"
+		*baseURL = envBaseURL
+		*baseURL += "/"
 	}
 	if envPath := os.Getenv("FILE_STORAGE_PATH"); envPath != "" {
 		*path = envPath
