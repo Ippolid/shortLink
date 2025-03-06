14098428858455c8215ab24bbc308d8616a37b07
Ippolid <iliamishin2006@gmail.com>
2025-03-06 18:04:59 +0300

    edit

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -8,6 +8,39 @@ import (
 	"net/http"
 )
 
+//	func AuthMiddleware() gin.HandlerFunc {
+//		return func(c *gin.Context) {
+//			authHeader := c.GetHeader("Authorization")
+//			var accessToken string
+//			if authHeader != "" {
+//				accessToken = authHeader
+//			} else {
+//				cookie, err := c.Cookie("user_id")
+//				if err == nil && cookie != "" {
+//					accessToken = cookie
+//				} else {
+//					accessToken = ""
+//				}
+//			}
+//
+//			userID, err := service.VerifyUser(accessToken)
+//			if err != nil {
+//				userID = uuid.New().String()
+//				token, err := service.CreatTokenForUser(userID)
+//				if err != nil {
+//					c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate auth token"})
+//					c.Abort()
+//					return
+//				}
+//
+//				c.SetCookie("user_id", token, 3600, "/", "", false, true)
+//				c.Header("Authorization", token)
+//			}
+//
+//			c.Set("user_id", userID)
+//			c.Next()
+//		}
+//	}
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
@@ -24,7 +57,7 @@ func AuthMiddleware() gin.HandlerFunc {
 		}
 
 		userID, err := service.VerifyUser(accessToken)
-		if err != nil {
+		if err != nil || accessToken == "" {
 			userID = uuid.New().String()
 			token, err := service.CreatTokenForUser(userID)
 			if err != nil {
